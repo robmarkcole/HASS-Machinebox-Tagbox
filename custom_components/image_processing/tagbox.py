3916bff8dd085721eeb4c6dccdf8d1578cda3e71
robmarkcole <robmarkcole@gmail.com>
2018-04-02 07:58:02 +0100

    Initial

---
+++ custom_components/image_processing/tagbox.py
@@ -0,0 +1,96 @@
+"""
+Component that will search images for tagged objects via a local
+machinebox instance.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/image_processing.tagbox
+"""
+import base64
+import requests
+import logging
+import voluptuous as vol
+
+from homeassistant.core import split_entity_id
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.image_processing import (
+    PLATFORM_SCHEMA, ImageProcessingEntity, CONF_SOURCE, CONF_ENTITY_ID,
+    CONF_NAME)
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_ENDPOINT = 'endpoint'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_ENDPOINT): cv.string
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the classifier."""
+    entities = []
+    for camera in config[CONF_SOURCE]:
+        entities.append(Tagbox(
+            camera.get(CONF_NAME),
+            config[CONF_ENDPOINT],
+            camera[CONF_ENTITY_ID],
+        ))
+    add_devices(entities)
+
+
+class Tagbox(ImageProcessingEntity):
+    """Perform a tag search via a Tagbox."""
+
+    def __init__(self, name, endpoint, camera_entity):
+        """Init with the API key and model id"""
+        super().__init__()
+        if name:  # Since name is optional.
+            self._name = name
+        else:
+            self._name = "Tagbox {0}".format(
+                split_entity_id(camera_entity)[1])
+        self._camera = camera_entity
+        self._url = "http://{}/tagbox/check".format(endpoint)
+        self._state = None
+        self._attributes = {}
+
+    def process_image(self, image):
+        """Process an image."""
+        response = requests.post(
+            self._url,
+            json=self.encode_image(image)
+            ).json()
+
+        if response['success']:
+            self._state = response['tags'][0]['tag']
+            self._attributes = {
+                tag['tag']: round(tag['confidence'], 2)
+                for tag in response['tags']
+                }
+        else:
+            self._state = "Request_failed"
+            self._attributes = {}
+
+    def encode_image(self, image):
+        """base64 encode an image stream."""
+        base64_img = base64.b64encode(image).decode('ascii')
+        return {"base64": base64_img}
+
+    @property
+    def camera_entity(self):
+        """Return camera entity id from process pictures."""
+        return self._camera
+
+    @property
+    def device_state_attributes(self):
+        """Return other details about the sensor state."""
+        return self._attributes
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        return self._state
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
